9ce4006aabef3b5ba5742ec1c2c776f47a578bb6
mitch haesch <mitchhaesch@gmail.com>
2026-09-06 12:30:19 +0000

    init

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    allowedHosts: ['octopus'],
+  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
---
+++ src/config/home.ts
@@ -0,0 +1,42 @@
+export const sites = [
+  {
+    cat: 'Media',
+    urls: [
+      'https://bbc.com',
+      'https://nytimes.com',
+      'https://yahoo.co.jp',
+      'https://livedoor.com'
+    ]
+  },
+  {
+    cat: 'AI',
+    urls: [
+      'https://claude.ai',
+      'https://openai.com',
+      'https://dash.cloudflare.com',
+      'https://perplexity.ai'
+    ]
+  },
+  { cat: 'Repos', urls: ['https://github.com/dustytokyo'] },
+  {
+    cat: 'Clouds',
+    urls: [
+      'https://console.cloud.google.com',
+      'https://console.aws.amazon.com',
+      'https://portal.azure.com'
+    ]
+  },
+  {
+    cat: 'Develop',
+    urls: ['http://localhost:3000', 'http://octopus:3000']
+  },
+  {
+    cat: 'Misc',
+    urls: [
+      'https://youtube.com',
+      'https://amazon.co.jp',
+      'https://www.r-staffing.co.jp',
+      'https://www.r-staffing.co.jp/sol/op21/sd25'
+    ]
+  }
+];
